a5385d820b259188bd7095560ae89f7748b3a832
Azrubael <i0638464000@gmail.com>
2023-02-13 22:17:01 +0200

    Step #11. Components debugged. Only essentials left, old components removed

--- src/App.js
+++ src/App.js
@@ -11,41 +11,31 @@ function App() {
   const [ posts, setPosts ] = useState([
     {id: 1, title: "Javascript", body: "Description JavaScript"},
     {id: 2, title: "Go language", body: "Description Go language"},
-    {id: 3, title: "Pithon", body: "Description Pithon"}
+    {id: 3, title: "Python", body: "Description Python"}
   ])
 
-  const [ posts2, setPosts2 ] = useState([
-    {id: 1, title: "Java", body: "Description for Java"},
-    {id: 2, title: "Scala", body: "Description for Scala"},
-    {id: 3, title: "C#", body: "Description for C#"}
-  ])
+  const [ title, setTitle ] = useState('')
 
-  const [ title, setTitle ] = useState('I <3 programming')
-  const addNewPost = () => {
-
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
   }
-
 
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Post title" />&nbsp;
-        <input type="text" placeholder="Post description" />&nbsp;
-        <button>Create post</button>
-      </form>
-      <form>
       {/* Ниже показан управляемый компонент */}
         <MyInput 
           value={ title }
+          onChange={ e => setTitle(e.target.value) }
           type="text"
           placeholder="Post new title"
         />
         <MyInput type="text" placeholder="Post new description" />
-        <MyButton disabled={ true } onClick={ addNewPost }>Create post</MyButton>
+        <MyButton disabled={ false } onClick={ addNewPost }>Create post</MyButton>
       </form>
       <PostList posts={ posts } title="FIRST LIST OF POSTS"/>
       <br />
-      <PostList posts={ posts2 } title="SECOND LIST OF POSTS"/>
       <ClassCounter />
     </div>
   )
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,20 @@
 import React from "react"
 import PostItem from "./PostItem"
-import { Counter } from './Counter.jsx'
 
 
-const PostsList = ({posts, title}) => {
+const PostsList = ({ posts, title }) => {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>
         { title }
       </h1>
-      {posts.map(post => {
-        return (<div>
-          <Counter />
-          <PostItem post={ post } key={ post.id }/>
+      {posts.map( post => {
+        return ( <div>
+          <PostItem post={ post } key={ posts.id }/>
           <br />
         </div>
         )}
       )}
-      
     </div>
   )
 }
