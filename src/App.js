72cf69b3a08f3d0ddde897f50f9f73364886705e
Azrubael <i0638464000@gmail.com>
2023-02-17 12:23:24 +0200

    Step #21. Decomposition in 'PostFilter.jsx', edited 'PostsList.jsx'. Time 01:23:30

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useMemo } from 'react'
-import ClassCounter from './components/ClassCounter.jsx'
 import './styles/App.css'
 import PostList from './components/PostList.jsx'
 import PostForm from './components/UI/PostForm.jsx'
-import MySelect from './components/UI/select/MySelect.jsx'
-import MyInput from './components/UI/input/MyInput.jsx'
+import PostFilter from './components/PostFilter.jsx'
 
 
 function App() {
@@ -14,22 +12,21 @@ function App() {
     { id: 2, title: "Python", body: "aPython Description" },
     { id: 3, title: "Go language", body: "Go language Description" },
   ])
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+  const [filter, setFilter] = useState({sort: '', query: ''})
 
   const sortedPosts = useMemo(() => {
-    if (selectedSort) {
+    if (filter.sort) {
       // сортировка копии массива, а не массива напрямую
       // (если выбран параметр сортировки)
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
     }
     return posts
-  }, [selectedSort, posts])
+  }, [filter.sort, posts])
 
   const searchedAndSortedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
     //в массив зависимостей передается поисковая строка и отсортированный массив
-  }, [searchQuery, sortedPosts])
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -40,38 +37,15 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort)
-    // setPosts() // вроде можно удалить
-  }
-
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
-      <div>
-        <MyInput
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          placeholder="Search..."
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Sorting"
-          options={[
-            { value: 'title', name: 'By name' },
-            { value: 'body', name: 'By description' }
-          ]}
-        />
-      </div>
-      {searchedAndSortedPosts.length !== 0
-        ? <PostList remove={removePost} posts={searchedAndSortedPosts} title="THE LIST OF POSTS" />
-        : <div><br />
-            <h3 style={{textAlign: 'center'}}>Posts not found</h3>
-          <ClassCounter />
-        </div>
-      }
+      <PostFilter 
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={searchedAndSortedPosts} title="THE LIST OF POSTS" />
     </div>
   )
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,20 @@
 import React from "react"
 import PostItem from "./PostItem"
+import ClassCounter from "./ClassCounter"
 
 
 const PostsList = ({ posts, title, remove }) => {
+
+  if (!posts.length) {
+    return (
+        <div>
+          <br />
+          <h3 style={{textAlign: 'center'}}>Posts not found</h3>
+          <ClassCounter />
+        </div>
+    )
+  }
+
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>
