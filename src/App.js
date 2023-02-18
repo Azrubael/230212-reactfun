b40bc0f85754db3c964ec72140ca445de23a9061
Azrubael <i0638464000@gmail.com>
2023-02-18 23:06:03 +0200

    Step #25. Async 'fetchPosts' and simple dashedsircle during loading, with 1 second timeout. Time 01:48:00

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles/App.css'
 import PostList from './components/PostList.jsx'
-import PostForm from './components/UI/PostForm.jsx'
+import PostForm from './components/PostForm.jsx'
 import PostFilter from './components/PostFilter.jsx'
 import MyModal from './components/UI/MyModal/MyModal'
 import MyButton from './components/UI/button/MyButton'
 import { usePosts } from './hooks/usePosts'
-
+import PostService from './API/PostService'
+import Loader from './components/UI/Loader/Loader'
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "JavaScript Description" },
-    { id: 2, title: "Python", body: "aPython Description" },
-    { id: 3, title: "Go language", body: "Go language Description" },
-  ])
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
   const searchedAndSortedPosts = usePosts(posts, filter.sort, filter.query)
+  const [isPostsLoading, setIsPostsLoading] = useState(false)
 
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
   }
+
+  async function fetchPosts() {
+    setIsPostsLoading(true)
+    setTimeout( async () => {
+        const res = await PostService.getAll()
+        setPosts(res)
+        setIsPostsLoading(false)
+    }, 1000)
+  }
+  
 
   // Получаем post из дочернего компонента
   const removePost = (post) => {
@@ -32,6 +43,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* <button onClick={ fetchPosts }>GET POSTS</button> */}
       <MyButton style={{marginTop: 20}} onClick={() => setModal(true)}>
         Create a new post
       </MyButton>
@@ -43,7 +55,13 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList remove={removePost} posts={searchedAndSortedPosts} title="THE LIST OF POSTS" />
+      
+      { isPostsLoading 
+        ? <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Loader />
+          </div>
+        : <PostList remove={removePost} posts={searchedAndSortedPosts} title="THE LIST OF POSTS" />
+      }
     </div>
   )
 }
