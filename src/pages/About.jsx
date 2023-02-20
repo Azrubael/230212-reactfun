733ad8bc9f0f50d7d352e70d210e735f0909ad32
Azrubael <i0638464000@gmail.com>
2023-02-20 14:42:28 +0200

    Step #28. Added 'react-router-dom' with the new 'Rect.js' syntax. Added two routes: '/about' and '/posts'. Time 02:15:15

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <h3>
+      IT application was created as a part of the course "Программирование на JavaScript"
+    </h3>
+  )
+}
+
+export default About
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import PostList from '../components/PostList.jsx'
+import PostForm from '../components/PostForm.jsx'
+import PostFilter from '../components/PostFilter.jsx'
+import MyModal from '../components/UI/MyModal/MyModal'
+import MyButton from '../components/UI/button/MyButton'
+import { usePosts } from '../hooks/usePosts'
+import PostService from '../API/PostService'
+import Loader from '../components/UI/Loader/Loader'
+import { useFetching } from '../hooks/useFetching'
+import { getPageCount } from '../utils/pages'
+import Pagination from '../components/UI/pagination/Pagination'
+
+function Posts() {
+
+  const [posts, setPosts] = useState([])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+  const searchedAndSortedPosts = usePosts(posts, filter.sort, filter.query)
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit,page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
+  })
+  
+  useEffect(() => { 
+    fetchPosts(limit, page)
+  }, [])      //    <<<<< ????????  'fetchPosts' и убрать 'page'
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+    setModal(false)
+  }
+
+  // Получаем post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+  }
+
+  return (
+    <div className="App">
+      <MyButton style={{marginTop: 20}} onClick={() => setModal(true)}>
+        Create a new post
+      </MyButton>
+      <MyModal visible={modal} setVisble={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter 
+        filter={filter}
+        setFilter={setFilter}
+      />
+      { postError &&
+        <h3 style={{justifyContent: 'center'}}>An error has occurred: ${postError}</h3>
+      }
+      { isPostsLoading 
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+          <Loader />
+          </div>
+        : <PostList remove={removePost} posts={searchedAndSortedPosts} title="THE LIST OF POSTS" />
+      }
+      <Pagination
+        page={page}
+        changePage={changePage}
+        totalPages={totalPages}
+      />
+    </div>
+  )
+}
+
+export default Posts
